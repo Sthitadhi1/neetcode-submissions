742300185b4290de54f9e954cc1edb5d9375b6ed
Sthitadhi Maity <137975462+Sthitadhi1@users.noreply.github.com>
2026-06-28 23:21:48 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> seen;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+
+                string row = "R" + to_string(i) + board[i][j];
+                string col = "C" + to_string(j) + board[i][j];
+                string box = "B" + to_string(i / 3) + to_string(j / 3) + board[i][j];
+
+                if (seen.count(row) || seen.count(col) || seen.count(box))
+                    return false;
+
+                seen.insert(row);
+                seen.insert(col);
+                seen.insert(box);
+            }
+        }
+
+        return true;
+    }
+};
